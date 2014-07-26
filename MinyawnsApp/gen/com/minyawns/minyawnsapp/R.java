bd86f5d0e8ff1ff72f9d144c90b4ace26008ceb4
Ben Schoeggl <Ben@Benjamins-MacBook-Pro.local>
2014-07-25 22:35:49 -0700

    Deleting extra res/values/string files

--- MinyawnsApp/gen/com/minyawns/minyawnsapp/R.java
+++ MinyawnsApp/gen/com/minyawns/minyawnsapp/R.java
@@ -16,9 +16,6 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
-    public static final class array {
-        public static final int pages_array=0x7f0c0000;
-    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -1082,8 +1079,13 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int action_search=0x7f020057;
         public static final int drawer_shadow=0x7f020058;
-        public static final int ic_drawer=0x7f020059;
-        public static final int ic_launcher=0x7f02005a;
+        public static final int facebook_logo_100pi=0x7f020059;
+        public static final int facebook_logo_29pi=0x7f02005a;
+        public static final int green_button=0x7f02005b;
+        public static final int ic_drawer=0x7f02005c;
+        public static final int ic_launcher=0x7f02005d;
+        public static final int login_with_facebook_button=0x7f02005e;
+        public static final int text_box=0x7f02005f;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1097,30 +1099,37 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
+        public static final int already_have_account_button=0x7f050045;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int content_frame=0x7f05003d;
+        public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
-        public static final int drawer_layout=0x7f05003c;
+        public static final int dont_have_an_account_button=0x7f050042;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int first_name=0x7f050043;
+        public static final int forgot_password_button=0x7f050041;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int left_drawer=0x7f05003e;
+        public static final int last_name=0x7f050044;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int login_button=0x7f050040;
+        public static final int login_email=0x7f05003e;
+        public static final int login_password=0x7f05003f;
+        public static final int login_with_facebook_button=0x7f05003d;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1201,14 +1210,15 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
+        public static final int activity_login=0x7f030018;
         public static final int drawer_list_item=0x7f030019;
         public static final int fragment_jobs=0x7f03001a;
-        public static final int fragment_main=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int fragment_login=0x7f03001b;
+        public static final int fragment_signup=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
-        public static final int main=0x7f0d0000;
+        public static final int main=0x7f0c0000;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1251,11 +1261,22 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a0019;
+        public static final int already_have_account_button_text=0x7f0a0016;
+        public static final int already_have_an_account_button_text=0x7f0a0018;
         public static final int app_name=0x7f0a000d;
-        public static final int drawer_close=0x7f0a0011;
-        public static final int drawer_open=0x7f0a0010;
-        public static final int hello_world=0x7f0a000e;
+        public static final int dont_have_an_account_button_text=0x7f0a0017;
+        public static final int email_textbox_text=0x7f0a0010;
+        public static final int first_name_textbox_text=0x7f0a001b;
+        public static final int forgot_password_button_text=0x7f0a0015;
+        public static final int last_name_textbox_text=0x7f0a001c;
+        public static final int login_button_text=0x7f0a0012;
+        public static final int login_page_title=0x7f0a000e;
+        public static final int login_with_facebook_button_text=0x7f0a0013;
+        public static final int or_divider_text=0x7f0a000f;
+        public static final int password_textbox_text=0x7f0a0011;
+        public static final int signup_button_text=0x7f0a0014;
+        public static final int signup_page_title=0x7f0a001a;
     }
     public static final class style {
         /** 
